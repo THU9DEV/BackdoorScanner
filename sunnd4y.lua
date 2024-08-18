784ffa3c9963c5f43b963b9a6dfba6780f1e9619
sunnd4y <120350465+THU9DEV@users.noreply.github.com>
2024-08-18 08:09:17 +0700

    Update sunnd4y.lua

--- sunnd4y.lua
+++ sunnd4y.lua
@@ -1,6 +1,6 @@
 -- // Bypass Adonis
 
-Game_Log = true
+_G.Game_Log = getgenv().Game_Log and true or false
 
 for k,v in pairs(game:GetDescendants()) do if pcall(function() return rawget(v,"indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then v.tvk = {"kick",function() return false end} end end
 
